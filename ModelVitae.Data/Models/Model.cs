6086c5e778b0e75a8a7b32f52575ba9df4f57a41
Ferenc Dykstra <oneup1@gmail.com>
2021-06-15 14:54:10 +0200

    Add project files.

---
+++ ModelVitae.Data/Models/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+namespace ModelVitae.Data.Models
+{
+    public class Model
+    {​​​​​​​
+        public int Id {​​​​​​​ get; set; }​​​​​​​
+
+        public PersonalData PersonalData {​​​​​​​ get; set; }​​​​​​​
+
+        public Gender Gender {​​​​​​​ get; set; }​​​​​​​
+
+        public DateTime DateOfBirth { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Height {​​​​​​​ get; set; }​​​​​​​
+
+        public double Waist {​​​​​​​ get; set; }​​​​​​​
+
+        public byte[] Image { get; set; }
+
+        public string PhoneNumber {​​​​​​​ get; set; }​​​​​​​
+
+        public string EmailAdress {​​​​​​​ get; set; }​​​​​​​
+
+        public EyeColor EyeColor {​​​​​​​ get; set; }​​​​​​​
+
+        public Color HairColor {​​​​​​​ get; set; }​​​​​​​
+    }​​​​​​​
+}
---
+++ ModelVitae.Data/Models/PersonalData.cs
@@ -0,0 +1,30 @@
+﻿namespace ModelVitae.Data.Models
+{
+    public class PersonalData
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+    }
+
+    public enum Gender
+    {​​​​​​​
+        Male,
+        Female,
+        Other
+    }​​​​​​​
+
+    public enum EyeColor
+    {​​​​​​​
+        Amber,
+        Blue,
+        Brown,
+        Gray,
+        Green,
+        Hazel,
+        Heterochromatic,
+        Other
+    }​​​​​​​
+}
